d3b68511c13068ccdbf5e3872e58ebfb403c8a79
Mujo <mbabajic.babajic56@gmail.com>
2023-10-24 13:54:54 +0200

    defined initial design

---
+++ dom.js
@@ -0,0 +1,15 @@
+//ENCODER REFERENCES
+const encoderInput = document.querySelector("#encoder-text-input");
+const encoderAlgorithmSelect = document.querySelector("#algorithm-select");
+const caesarShiftInput = document.querySelector("#caesar-shift-input");
+const vigenereKeywordInput = document.querySelector("#vigenere-keyword-input");
+const encoderOutput = document.querySelector("#encoder-text-output");
+const encodeButton = document.querySelector("#encode-button");
+
+//DECODER REFERENCES
+const decoderInput = document.querySelector("#decoder-text-input");
+const decoderDetectorOutput = document.querySelector(
+  "#decoder-detector-output"
+);
+const decoderOutput = document.querySelector("#decoder-text-output");
+const decodeButton = document.querySelector("#decode-button");
